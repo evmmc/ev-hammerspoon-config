316c04f271a8d8cfee9f626e3f5ec8b90f428916
ev <ev@evelyn.bz>
2026-01-14 17:13:27 +0100

    upd

--- utils.lua
+++ utils.lua
@@ -1,5 +1,5 @@
 -- utils.lua
- 
+
 -- List all running apps
 function listAllApps()
     local apps = hs.application.runningApplications() -- running apps
@@ -18,24 +18,24 @@ function listAllApps()
 end
 
 -- Empty Trash instantly, system-wide
-hs.hotkey.bind({"cmd", "alt", "shift"}, "delete", function()
+hs.hotkey.bind({ "cmd", "option", "ctrl" }, "delete", function()
     hs.osascript.applescript([[
         tell application "Finder"
             empty the trash without asking
         end tell
     ]])
-    hs.notify.new({title="Hammerspoon", informativeText="Trash Emptied Instantly"}):send()
+    hs.notify.new({ title = "Hammerspoon", informativeText = "Trash Emptied Instantly" }):send()
 end)
 
 -- Show the name of the active application while switching
 hs.application.watcher.new(function(appName, eventType, app)
     if eventType == hs.application.watcher.activated then
         hs.alert.show("Active Application: " .. appName)
-        print("Active Application: " .. appName)  -- Logs to Hammerspoon console
+        print("Active Application: " .. appName) -- Logs to Hammerspoon console
     end
 end):start()
 
 -- Toggle Hammerspoon Console with Cmd + Option + Y
-hs.hotkey.bind({"cmd", "option"}, "5", function()
+hs.hotkey.bind({ "cmd", "option" }, "5", function()
     hs.toggleConsole()
 end)
